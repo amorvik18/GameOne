cde658f1c5efc9a1ab8de9638b52a3a52666234d
amorvik18 <dabrowski.kamil18@gmail.com>
2020-04-19 07:54:40 +0000

    refactoring

--- main.cs
+++ main.cs
@@ -3,8 +3,8 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine("Hello Stranger! Please tell me your name.");
-    string name = Console.ReadLine();
-    Console.WriteLine ($"Welcome in GameOne {name}. Please press enter to start the game.");
+    string userName = Console.ReadLine();
+    Console.WriteLine ($"Welcome in GameOne {userName}. Press enter to continue...");
     Console.ReadLine();
   }
 }
