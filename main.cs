91e4423f31f875429944a0423c88cfa127423d76
amorvik18 <dabrowski.kamil18@gmail.com>
2020-04-21 09:52:38 +0000

    add namespace - Snake

--- main.cs
+++ main.cs
@@ -1,10 +1,14 @@
 using System;
 
+namespace Snake 
+{
 class MainClass {
-  public static void Main (string[] args) {
+    public static void Main (string[] args) {
+    
     Console.WriteLine("Hello Stranger! Please tell me your name.");
     string userName = Console.ReadLine();
     Console.WriteLine ($"Welcome in GameOne {userName}. Press enter to continue...");
     Console.ReadLine();
+    }
   }
 }
