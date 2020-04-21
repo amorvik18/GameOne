e41cef26f0f80be1a43746df92970d23cff2ee1b
amorvik18 <dabrowski.kamil18@gmail.com>
2020-04-21 10:17:10 +0000

    add first 'if' statement, when the user selects 'no''

--- main.cs
+++ main.cs
@@ -13,8 +13,15 @@ namespace GameOne
     Console.WriteLine($"Hello, {name}! Welcome to our story.");
     Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
     Console.Write("Please type \"YES\" or \"NO\": ");
+    
     string noiseChoice = Console.ReadLine();
     noiseChoice = noiseChoice.ToUpper();
+      
+      if (noiseChoice == "NO")
+      {
+      Console.WriteLine("Not much of an adventure if we don't leave our room! \n THE END.\n Press any key to close the game.");
+      Console.ReadLine();
+      }
     }
   }
 }
