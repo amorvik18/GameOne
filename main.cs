620bc3a3bc0de427d53f37efc5833f7077aa9fc6
amorvik18 <dabrowski.kamil18@gmail.com>
2020-04-21 10:10:32 +0000

    add var name, add two sentences ans another question to user

--- main.cs
+++ main.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace GameOne 
 {
-  class MainClass {
-    static void Main (string[] args) {
+  class MainClass 
+  {
+    static void Main (string[] args) 
+    {
     // Start by asking fo the user's name:
+    
     Console.Write("What is your name?: ");
-
+    string name = Console.ReadLine();
+    Console.WriteLine($"Hello, {name}! Welcome to our story.");
+    Console.WriteLine("It begins on a cold rainy night. You're sitting in your room and hear a noise coming from down the hall. Do you go investigate?");
+    Console.Write("Please type \"YES\" or \"NO\": ");
     }
   }
 }
